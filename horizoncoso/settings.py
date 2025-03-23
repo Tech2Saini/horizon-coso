bbeb5b4220c0d792ddf62005f3b99dabe0507087
tech2saini <saini123sonu1@gmail.com>
2025-03-24 02:23:04 +0530

    add projection dabase

--- horizoncoso/settings.py
+++ horizoncoso/settings.py
@@ -79,11 +79,15 @@ WSGI_APPLICATION = 'horizoncoso.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# postgresql://postgres:LCrekmlDcoEHryndfUGrPELghBLtagRm@switchyard.proxy.rlwy.net:56992/railway
+
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=dj_database_url.config(default=os.getenv("DATABASE_URL")),
+        conn_max_age=600,
+    )
 }
 
 
