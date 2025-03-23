1a33011c8c4e01e5ac2395159d25124ac3a77202
tech2saini <saini123sonu1@gmail.com>
2025-03-23 23:56:14 +0530

    removing all other urls

--- home/urls.py
+++ home/urls.py
@@ -9,13 +9,13 @@ from .views import contact_view,askQuestions,answerQuestions,questionAction,user
 
 urlpatterns = [
     path('',home,name='home'),
-    re_path(r'^favicon\.ico$', RedirectView.as_view(url='/static/img/favicon.ico', permanent=True)),
-    path('contact/', contact_view, name='contact'),
-    path("ckeditor/", include('ckeditor_uploader.urls')), # <-- here
-    path("privacy-policy/",privacyPolicy, name="privacy_policy"),
-    path("terms-conditions/",termsConditions, name="terms_condition"),
-    path('ask-questions/',askQuestions,name='questions'),
-    path('answer-questions/your-answer/<int:id>/',userAnswer,name='user_answer'),
-    path('answer-questions/',answerQuestions,name='answers'),
-    path('answer-questions/<int:n>/<str:action>/',questionAction,name='answer_action'),
+    # re_path(r'^favicon\.ico$', RedirectView.as_view(url='/static/img/favicon.ico', permanent=True)),
+    # path('contact/', contact_view, name='contact'),
+    # path("ckeditor/", include('ckeditor_uploader.urls')), # <-- here
+    # path("privacy-policy/",privacyPolicy, name="privacy_policy"),
+    # path("terms-conditions/",termsConditions, name="terms_condition"),
+    # path('ask-questions/',askQuestions,name='questions'),
+    # path('answer-questions/your-answer/<int:id>/',userAnswer,name='user_answer'),
+    # path('answer-questions/',answerQuestions,name='answers'),
+    # path('answer-questions/<int:n>/<str:action>/',questionAction,name='answer_action'),
 ]
