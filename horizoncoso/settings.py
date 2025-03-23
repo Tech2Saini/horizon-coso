fbd49bd5fddbd244255e46fd0e89fad9bcc476b4
tech2saini <saini123sonu1@gmail.com>
2025-03-24 02:32:47 +0530

    new database

--- horizoncoso/settings.py
+++ horizoncoso/settings.py
@@ -78,13 +78,15 @@ WSGI_APPLICATION = 'horizoncoso.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default="postgresql://postgres:LCrekmlDcoEHryndfUGrPELghBLtagRm@switchyard.proxy.rlwy.net:56992/railway",
+        conn_max_age=600,
+    )
 }
+
 
 
 # Password validation
