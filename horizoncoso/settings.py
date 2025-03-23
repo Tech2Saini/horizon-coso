fb3d5fe2b1931ca0715133cb2c13e5cd0440004f
tech2saini <saini123sonu1@gmail.com>
2025-03-24 02:29:39 +0530

    remove new database

--- horizoncoso/settings.py
+++ horizoncoso/settings.py
@@ -79,15 +79,11 @@ WSGI_APPLICATION = 'horizoncoso.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# postgresql://postgres:LCrekmlDcoEHryndfUGrPELghBLtagRm@switchyard.proxy.rlwy.net:56992/railway
-
-import dj_database_url
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default="postgresql://postgres:LCrekmlDcoEHryndfUGrPELghBLtagRm@switchyard.proxy.rlwy.net:56992/railway",
-        conn_max_age=600,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
